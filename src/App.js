5a8d875a1a68445a61f15d19c47b3bfd94678390
vednoob <leduchyy@gmail.com>
2023-02-22 18:30:14 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter, Route, Switch, Redirect, Link } from 'react-router-dom';
-import './App.scss';
-import NotFound from './components/NotFound';
+import React, { Suspense } from "react";
+import { BrowserRouter, Route, Switch, Redirect, Link } from "react-router-dom";
+import "./App.scss";
+import NotFound from "./components/NotFound";
 
 // Lazy load - Code splitting
-const Photo = React.lazy(() => import('./features/Photo'));
+const Photo = React.lazy(() => import("./features/Photo"));
 
 function App() {
   return (
     <div className="photo-app">
       <Suspense fallback={<div>Loading ...</div>}>
         <BrowserRouter>
-
-          {/* TODO: Remove after testing */}
+          {/* TODO: Remove after testingg */}
           <ul>
-            <li><Link to="/photo">Go to photo page</Link></li>
-            <li><Link to="/photo/add">Go to Add new photo page</Link></li>
-            <li><Link to="/photo/123">Go to Edit photo page</Link></li>
+            <li>
+              <Link to="/photo">Go to photo page</Link>
+            </li>
+            <li>
+              <Link to="/photo/add">Go to Add new photo page</Link>
+            </li>
+            <li>
+              <Link to="/photo/123">Go to Edit photo page</Link>
+            </li>
           </ul>
 
           <Switch>
